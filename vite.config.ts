c6b63d9d1d58926dd5453a279f47f93e21424970
Sajid <95769563+sahmed0@users.noreply.github.com>
2026-03-24 11:45:28 +0000

    vite.config.ts base update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: '/heatmap/',
+  base: './',
   server: {
     headers: {
       // Required for SharedArrayBuffer (The "High Performance" requirement)
