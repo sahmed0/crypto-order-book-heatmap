7f6d8639f9d66ac8d37138d030f9e29d7c47cbd1
Sajid <95769563+sahmed0@users.noreply.github.com>
2026-04-28 20:36:57 +0100

    improved header

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ function App() {
     <div class={styles.appContainer}>
       <header class={styles.header}>
         <h1 class={styles.title}>
+          <svg style={{ height: "2rem", width: "2rem" }} fill="#F0B90B" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Binance</title><path d="M16.624 13.9202l2.7175 2.7154-7.353 7.353-7.353-7.352 2.7175-2.7164 4.6355 4.6595 4.6356-4.6595zm4.6366-4.6366L24 12l-2.7154 2.7164L18.5682 12l2.6924-2.7164zm-9.272.001l2.7163 2.6914-2.7164 2.7174v-.001L9.2721 12l2.7164-2.7154zm-9.2722-.001L5.4088 12l-2.6914 2.6924L0 12l2.7164-2.7164zM11.9885.0115l7.353 7.329-2.7174 2.7154-4.6356-4.6356-4.6355 4.6595-2.7174-2.7154 7.353-7.353z" /></svg>
           <span class={styles.symbol}>{activeSymbol().replace('USDT', '/USDT')}</span>{' '}
           <span class={styles.titleText}>Binance Order Book Heatmap</span>
         </h1>
 
         <div class={styles.nav}>
-          <a 
-            href="https://github.com/sahmed0/crypto-order-book-heatmap.git" 
-            target="_blank" 
+          <a
+            href="https://github.com/sahmed0/crypto-order-book-heatmap.git"
+            target="_blank"
             rel="noopener noreferrer"
             class={styles.githubLink}
           >
@@ -24,7 +25,7 @@ function App() {
           </a>
           <div class={styles.divider} />
           <span class={styles.copyright}>
-            &copy; 2026 Sajid Ahmed
+            &copy; 2026 Sajid Ahmed<br />All Rights Reserved
           </span>
         </div>
       </header>
